dc680a21f81dc59e460496af5472e1a1305d4995
Toma <tomasajtofficial@gmail.com>
2022-03-29 23:30:05 +0200

    Remake original functionality

--- TWinActivator/Form1.Designer.cs
+++ TWinActivator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TWinActivator
 {
-    partial class Form1
+    partial class TWinForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,34 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.keysComboBox = new System.Windows.Forms.ComboBox();
+            this.activate_button = new System.Windows.Forms.Button();
+            this.txtBx_kms = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // keysComboBox
             // 
-            this.comboBox1.DisplayMember = "Key";
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(297, 28);
-            this.comboBox1.TabIndex = 0;
-            this.comboBox1.ValueMember = "Value";
+            this.keysComboBox.DisplayMember = "Key";
+            this.keysComboBox.FormattingEnabled = true;
+            this.keysComboBox.Location = new System.Drawing.Point(4, 4);
+            this.keysComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.keysComboBox.Name = "keysComboBox";
+            this.keysComboBox.Size = new System.Drawing.Size(378, 33);
+            this.keysComboBox.TabIndex = 0;
+            this.keysComboBox.ValueMember = "Value";
             // 
-            // Form1
+            // activate_button
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.activate_button.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.activate_button.Location = new System.Drawing.Point(4, 81);
+            this.activate_button.Name = "activate_button";
+            this.activate_button.Size = new System.Drawing.Size(378, 86);
+            this.activate_button.TabIndex = 1;
+            this.activate_button.Text = "Activate";
+            this.activate_button.UseVisualStyleBackColor = true;
+            this.activate_button.Click += new System.EventHandler(this.PromptActivate);
+            // 
+            // txtBx_kms
+            // 
+            this.txtBx_kms.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.txtBx_kms.Location = new System.Drawing.Point(4, 44);
+            this.txtBx_kms.Name = "txtBx_kms";
+            this.txtBx_kms.Size = new System.Drawing.Size(378, 31);
+            this.txtBx_kms.TabIndex = 2;
+            this.txtBx_kms.Text = "KMS server";
+            this.txtBx_kms.Enter += new System.EventHandler(this.KMSTextBoxFocusEntered);
+            this.txtBx_kms.Leave += new System.EventHandler(this.KMSTextBoxFocusLeft);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.panel1.Controls.Add(this.keysComboBox);
+            this.panel1.Controls.Add(this.txtBx_kms);
+            this.panel1.Controls.Add(this.activate_button);
+            this.panel1.Location = new System.Drawing.Point(13, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(386, 177);
+            this.panel1.TabIndex = 3;
+            // 
+            // TWinForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(411, 194);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "TWinForm";
+            this.RightToLeftLayout = true;
             this.Text = "TWin Activator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private ComboBox comboBox1;
+        private ComboBox keysComboBox;
+        private Button activate_button;
+        private TextBox txtBx_kms;
+        private Panel panel1;
     }
 }
--- TWinActivator/Form1.cs
+++ TWinActivator/Form1.cs
@@ -1,53 +1,77 @@
-using System.ComponentModel;
 using System.Diagnostics;
 
-namespace TWinActivator
+namespace TWinActivator;
+public partial class TWinForm : Form
 {
-    public partial class Form1 : Form
+    private static readonly Dictionary<string, string> _dict = new()
     {
-        private static readonly Dictionary<string, string> _dict = new()
+        { "Windows 10/11 Pro", "W269N-WFGWX-YVC9B-4J6C9-T83GX" },
+        { "Windows 10/11 Pro N", "MH37W-N47XK-V7XM9-C7227-GCQG9" },
+        { "Windows 10/11 Pro for Workstations", "NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J" },
+        { "Windows 10/11 Pro for Workstations N", "9FNHH-K3HBT-3W4TD-6383H-6XYWF" },
+        { "Windows 10/11 Pro Education", "6TP4R-GNPTD-KYYHQ-7B7DP-J447Y" },
+        { "Windows 10/11 Pro Education N", "YVWGF-BXNMC-HTQYQ-CPQ99-66QFC" },
+        { "Windows 10/11 Education", "NW6C2-QMPVW-D7KKK-3GKT6-VCFB2" },
+        { "Windows 10/11 Education N", " 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ" },
+        { "Windows 10/11 Pro Enterprise", "NPPR9-FWDCX-D2C8J-H872K-2YT43" },
+        { "Windows 10/11 Pro Enterprise N", "DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4" },
+        { "Windows 10/11 Pro Enterprise G", "YYVX9-NTFWV-6MDM3-9PT4T-4M68B" },
+        { "Windows 10/11 Pro Enterprise G N", "44RPN-FTY23-9VTTB-MP9BX-T84FV" },
+        { "Windows 10/11 Home", "TX9XD-98N7V-6WMQ6-BX7FG-H8Q99" },
+        { "Windows 10/11 Home N", "3KHY7-WNT83-DGQKR-F7HPR-844BM" },
+    };
+    public TWinForm()
+    {
+        InitializeComponent();
+        keysComboBox.DataSource = new BindingSource(_dict, null);
+    }
+    public bool ActivateWindows()
+    {
+        try
         {
-            { "Windows 10/11 Home", "TX9XD-98N7V-6WMQ6-BX7FG-H8Q99" },
-            { "Windows 10/11 Home N", "3KHY7-WNT83-DGQKR-F7HPR-844BM" },
-            { "Windows 10/11 Pro", "W269N-WFGWX-YVC9B-4J6C9-T83GX" },
-            { "Windows 10/11 Pro N", "MH37W-N47XK-V7XM9-C7227-GCQG9" },
-            { "Windows 10/11 Pro for Workstations", "NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J" },
-            { "Windows 10/11 Pro for Workstations N", "9FNHH-K3HBT-3W4TD-6383H-6XYWF" },
-            { "Windows 10/11 Pro Education", "6TP4R-GNPTD-KYYHQ-7B7DP-J447Y" },
-            { "Windows 10/11 Pro Education N", "YVWGF-BXNMC-HTQYQ-CPQ99-66QFC" },
-            { "Windows 10/11 Education", "NW6C2-QMPVW-D7KKK-3GKT6-VCFB2" },
-            { "Windows 10/11 Education N", " 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ" },
-            { "Windows 10/11 Pro Enterprise", "NPPR9-FWDCX-D2C8J-H872K-2YT43" },
-            { "Windows 10/11 Pro Enterprise N", "DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4" },
-            { "Windows 10/11 Pro Enterprise G", "YYVX9-NTFWV-6MDM3-9PT4T-4M68B" },
-            { "Windows 10/11 Pro Enterprise G N", "44RPN-FTY23-9VTTB-MP9BX-T84FV" },
-        };
-        public Form1()
-        {
-            InitializeComponent();
-            comboBox1.DataSource = new BindingSource(_dict, null);
-        }
-        public static bool Activate()
-        {
-            //try
+            var proc = new Process
             {
-
-                new Process
-                {
-                    StartInfo = {
+                StartInfo = {
                         FileName = "cmd",
-                        Arguments = "/C \"slmgr \"",
+                        // I know it's not escaped properly, but what's the worst that could happen?
+                        Arguments = $"/C \"slmgr /skms {txtBx_kms.Text} & slmgr /ipk {keysComboBox.SelectedValue}\"",
                         UseShellExecute = true,
                         WindowStyle = ProcessWindowStyle.Hidden,
+                        Verb = "runas"
                     }
-                }.Start();
-                return true;
-            }
-            /*catch
-            {
-            }*/
-            return false;
+            };
+            proc.Start();
+            proc.WaitForExit();
+            return true;
         }
+        catch { }
+        return false;
+    }
 
+    private void PromptActivate(object sender, EventArgs e)
+    {
+        if (txtBx_kms.Text == "KMS server")
+        {
+            MessageBox.Show("Please fill out the KMS server box");
+            return;
+        }
+        if (MessageBox.Show(
+            $"Are you sure you want to activate Windows?\nKMS server: {txtBx_kms.Text}\nActivation code: {keysComboBox.SelectedValue}",
+            "Are you sure?", MessageBoxButtons.YesNo) != DialogResult.Yes) return;
+        if (ActivateWindows()) Application.Exit();
+    }
+
+    private void KMSTextBoxFocusEntered(object sender, EventArgs e)
+    {
+        if (txtBx_kms.Text != "KMS server") return;
+        txtBx_kms.Text = "";
+        txtBx_kms.ForeColor = SystemColors.WindowText;
+    }
+
+    private void KMSTextBoxFocusLeft(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(txtBx_kms.Text)) return;
+        txtBx_kms.Text = "KMS server";
+        txtBx_kms.ForeColor = SystemColors.GrayText;
     }
 }
